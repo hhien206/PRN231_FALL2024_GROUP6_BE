5c0a0f5945dbf64e6bfc1d0c19b9da8c186005a5
Hien Nguyen <hhien204@gmail.com>
2024-09-20 23:17:01 +0700

    Add project files.

---
+++ PRN231_FALL2024_GROUP6_API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace PRN231_FALL2024_GROUP6_API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
