47af66c41d8bb49989be4a592e12ecf2d6d9eab8
Hien Nguyen <hhien204@gmail.com>
2024-10-04 21:19:16 +0700

    Category template config

---
+++ PRN231_FALL2024-GROUP6_FE/Pages/job-list.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN231_FALL2024_GROUP6_FE.Pages
+{
+    public class job_listModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
