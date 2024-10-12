f5361a65bb148209c8af41b1cd6971bf90bcbb41
Hien Nguyen <hhien204@gmail.com>
2024-10-12 13:35:32 +0700

    fix job list + hob detail

---
+++ PRN231_FALL2024-GROUP6_FE/Pages/JobDetail.cshtml.cs
@@ -0,0 +1,31 @@
+using BusinessObject.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN231_FALL2024_GROUP6_FE.Pages
+{
+    public class JobDetailModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+
+        public JobDetailModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public JobView Job { get; set; } = new JobView();
+
+        public async Task OnGetAsync(int jobId)
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7170/api/Job/{jobId}");
+            if (response.IsSuccessStatusCode)
+            {
+                Job = await response.Content.ReadFromJsonAsync<JobView>();
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
