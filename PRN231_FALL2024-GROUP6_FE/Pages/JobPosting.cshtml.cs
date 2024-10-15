a9bf4cc634e50514a733d323a1f7af95a7b24891
Hien Nguyen <hhien204@gmail.com>
2024-10-15 17:42:03 +0700

    Job posting

---
+++ PRN231_FALL2024-GROUP6_FE/Pages/JobPosting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN231_FALL2024_GROUP6_FE.Pages
+{
+    public class JobPostingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
