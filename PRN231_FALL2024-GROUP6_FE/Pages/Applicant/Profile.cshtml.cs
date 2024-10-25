62873799d40b2d46512b1fe36fb391b6dd56c228
Hien Nguyen <hhien204@gmail.com>
2024-10-25 18:09:46 +0700

    profile

--- PRN231_FALL2024-GROUP6_FE/Pages/Applicant/Profile.cshtml.cs
+++ PRN231_FALL2024-GROUP6_FE/Pages/Applicant/Profile.cshtml.cs
@@ -1,26 +1,32 @@
+using BusinessObject.AddModel;
 using BusinessObject.ViewModel;
+using DataAccessObject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Newtonsoft.Json;
+using Service.Service;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace PRN231_FALL2024_GROUP6_FE.Pages.Applicant
 {
     public class ProfileModel : PageModel
     {
         private readonly ILogger<ProfileModel> _logger;
-        private readonly IHttpClientFactory _clientFactory;
+        private readonly HttpClient _httpClient;
 
-        public ProfileModel(ILogger<ProfileModel> logger, IHttpClientFactory clientFactory)
+        public ProfileModel(ILogger<ProfileModel> logger, HttpClient httpClient)
         {
             _logger = logger;
-            _clientFactory = clientFactory;
+            _httpClient = httpClient;
         }
+        [BindProperty]
+        public AccountView UserProfile { get; set; } = new AccountView();
 
-        public AccountView UserProfile { get; set; } 
+        public AccountJobSkillAdd  jobSkillAdd { get; set; } = new AccountJobSkillAdd();
 
         public async Task<IActionResult> OnGetAsync()
         {
-			// L?y accountId t? session
 			var accountId = HttpContext.Session.GetString("AccountId");
 
 			if (accountId == null)
@@ -28,19 +34,41 @@ namespace PRN231_FALL2024_GROUP6_FE.Pages.Applicant
                 return RedirectToPage("/Index");
             }
 
-            // G?i API ð? l?y thông tin ngý?i dùng theo accountId
-            var client = _clientFactory.CreateClient();
-            var response = await client.GetAsync($"https://localhost:7008/api/Account/GetById?accountId={accountId}");
-
+            var response = await _httpClient.GetAsync($"https://localhost:7008/api/Account/GetById?accountId={accountId}");
             if (response.IsSuccessStatusCode)
             {
-                var content = await response.Content.ReadAsStringAsync();
-                UserProfile = JsonConvert.DeserializeObject<AccountView>(content);
+                var result = await response.Content.ReadFromJsonAsync<ServiceResult>();
+                if (result != null && result.Status == 200)
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    UserProfile = JsonSerializer.Deserialize<AccountView>(result.Data.ToString(), options);
+                }
             }
             else
             {
-                _logger.LogError("Failed to load user profile");
+                Console.WriteLine($"Error: {response.StatusCode}");
             }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var accountId = HttpContext.Session.GetString("AccountId");
+
+            var jsonContent = JsonSerializer.Serialize(jobSkillAdd);
+            Console.WriteLine(jsonContent);
+
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:7257/api/NewsArticle/Add", jobSkillAdd);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("/Applicant/Profile");
+            }
+
+            var errorMessage = await response.Content.ReadAsStringAsync();
+            ModelState.AddModelError(string.Empty, $"Có l?i x?y ra: {errorMessage}");
 
             return Page();
         }
