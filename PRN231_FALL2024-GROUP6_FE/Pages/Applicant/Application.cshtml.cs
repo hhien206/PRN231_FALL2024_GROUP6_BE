8916e3405d71ea61f87e54e74488a4bda58ae4c5
Hien Nguyen <hhien204@gmail.com>
2024-10-31 16:17:44 +0700

    view application

---
+++ PRN231_FALL2024-GROUP6_FE/Pages/Applicant/Application.cshtml.cs
@@ -0,0 +1,41 @@
+using BusinessObject.ViewModel;
+using DataAccessObject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Service.Service;
+using System.Text.Json;
+
+namespace PRN231_FALL2024_GROUP6_FE.Pages.Applicant
+{
+    public class ApplicationModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApplicationModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public List<Application> applications { get; set; } = new List<Application>();
+
+        public async Task OnGetAsync()
+        {
+            var accountId = HttpContext.Session.GetString("AccountId");
+
+            var response = await _httpClient.GetAsync($"https://localhost:7008/api/Application/ViewAccount?accountId={accountId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<ServiceResult>();
+                if (result != null && result.Status == 200)
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    applications = JsonSerializer.Deserialize<List<Application>>(result.Data.ToString(), options);
+                }
+            }
+        }
+    }
+}
