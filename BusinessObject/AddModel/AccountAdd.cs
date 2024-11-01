ca7f61ddf5d0c8633c9e28e874e2804c74414e9d
Hien Nguyen <hhien204@gmail.com>
2024-11-01 13:55:07 +0700

    fix regis + fix navbar + job detail + template + listteamplate+ view job apllication

--- BusinessObject/AddModel/AccountAdd.cs
+++ BusinessObject/AddModel/AccountAdd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,20 @@ namespace BusinessObject.AddModel
 {
     public class AccountAdd
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string? email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Password must be at least 6 characters long", MinimumLength = 6)]
         public string? password { get; set; }
+        [Required(ErrorMessage = "Full name is required")]
         public string? FullName { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Phone number must contain only digits")]
         public string? PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Address is required")]
         public string? Address { get; set; }
+        [Required(ErrorMessage = "Gender is required")]
         public string? gender { get; set; }
 
     }
