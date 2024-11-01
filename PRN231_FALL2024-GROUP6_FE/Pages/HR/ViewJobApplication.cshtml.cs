e696c1d376a9c37362d0a59f56df7a35e136191d
Hien Nguyen <hhien204@gmail.com>
2024-11-01 14:55:53 +0700

    job applycation

--- PRN231_FALL2024-GROUP6_FE/Pages/HR/ViewJobApplication.cshtml.cs
+++ PRN231_FALL2024-GROUP6_FE/Pages/HR/ViewJobApplication.cshtml.cs
@@ -1,12 +1,51 @@
+using BusinessObject.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN231_FALL2024_GROUP6_FE.Pages.Applicant;
+using Service.Service;
+using System.Text.Json;
 
 namespace PRN231_FALL2024_GROUP6_FE.Pages.HR
 {
     public class ViewJobApplicationModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<ProfileModel> _logger;
+        private readonly HttpClient _httpClient;
+
+        public ViewJobApplicationModel(ILogger<ProfileModel> logger, HttpClient httpClient)
         {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+        public List<JobView> Jobs { get; set; } = new List<JobView>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var accountId = HttpContext.Session.GetString("AccountId");
+
+            if (accountId == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var response = await _httpClient.GetAsync($"https://localhost:7008/api/Job/ViewAllAccount?accountId={accountId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<ServiceResult>();
+                if (result != null && result.Status == 200)
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    Jobs = JsonSerializer.Deserialize<List<JobView>>(result.Data.ToString(), options);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+            return Page();
         }
     }
 }
